38f68bac90269a5ef94d2805518b293dde331547
piotrekpasciak <piotrek.pasciak@gmail.com>
2020-02-14 10:12:35 +0100

    Test

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,18 +1,18 @@
 require 'sidekiq'
 
-Sidekiq.configure_client do |config|
-  config.redis = { size: 1 }
-end
+# Sidekiq.configure_client do |config|
+#   config.redis = { size: 1 }
+# end
 
-Sidekiq.default_worker_options = {
-  'backtrace' => true,
-  'retry'     => false
-}
+# Sidekiq.default_worker_options = {
+#   'backtrace' => true,
+#   'retry'     => false
+# }
 
-schedule_file = "config/schedule.yml"
-Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) # && Sidekiq.server?
+# schedule_file = "config/schedule.yml"
+# Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file) if File.exist?(schedule_file) # && Sidekiq.server?
 
-unless Rails.env.test?
-  Sidekiq::Logging.logger.level = Logger::DEBUG
-  Rails.logger                  = Sidekiq::Logging.logger
-end
+# unless Rails.env.test?
+#   Sidekiq::Logging.logger.level = Logger::DEBUG
+#   Rails.logger                  = Sidekiq::Logging.logger
+# end
